1233a510410ad121c5632faf22a3f84a91bd18e5
luisHdz <luishernandez1611681@gmail.com>
2024-08-13 11:09:19 -0600

    Added functionality menu to console

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const FileService = require("./class/fileservice");
+const { askQuestion, readlineClouse } = require('./utils/questionUtils');
 
 
 const main = async () => {
@@ -9,6 +10,26 @@ const main = async () => {
 		// Crear el archivo si no existe
 		await fileService.createFile()
 
+		let exit = false;
+
+		while(!exit) {
+			console.log('***************************************');
+			const operation = await askQuestion(`Selecciona una opción:\n0.-Salir\n1.-Agregar tarea\n`);
+
+			switch(operation){
+				case '0':
+					exit = true;
+					break;
+				case '1': 
+					console.log("Agrega un nuevo valor");
+					break;
+				default:
+					console.log('Operación no válida');
+					break;
+			}
+		}
+
+		readlineClouse();
 
 	} catch (error) {
 		console.log('Ocurrio un error: ', error);
---
+++ utils/questionUtils.js
@@ -0,0 +1,17 @@
+const readline = require('readline')
+
+// Crear una interfaz readline
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// Función para pedir información
+const askQuestion = (query) => new Promise(resolve => rl.question(query, resolve));
+
+const readlineClouse = () => rl.close();
+
+module.exports = {
+    askQuestion,
+    readlineClouse
+}
